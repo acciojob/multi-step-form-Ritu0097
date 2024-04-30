ad8ee004b0d1e60d549eed39a17f1ac9d82da23f
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-30 15:40:06 +0530

    car

--- src/components/Step.js
+++ src/components/Step.js
@@ -1,55 +1,57 @@
 // Step.js
-import React from 'react'
+import React from 'react';
+import './Step.css';
+
 const Step = ({ step, formData, setFormData, onNext, onPrev, onSubmit }) => {
-	const handleChange = (e) => {
-		const { id, value } = e.target
-		setFormData({ ...formData, [id]: value })
-	}
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		if (step === 3) {
-			onSubmit()
-		} else {
-			onNext()
-		}
-	}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (step === 3) {
+      onSubmit();
+    } else {
+      onNext();
+    }
+  };
 
-	return (
-		<div id={`step${step}`} className="step-container">
-			<h2>Step {step}</h2>
-			<form onSubmit={handleSubmit}>
-				<div className="input-group">
-					<input
-						type="text"
-						id="first_name"
-						value={formData.first_name}
-						onChange={handleChange}
-						placeholder="First Name"
-					/>
-				</div>
-				<div className="input-group">
-					<input
-						type="text"
-						id="last_name"
-						value={formData.last_name}
-						onChange={handleChange}
-						placeholder="Last Name"
-					/>
-				</div>
-				{step !== 1 && (
-					<button type="button" onClick={onPrev}>
-						Previous
-					</button>
-				)}
-				{step !== 3 ? (
-					<button type="submit">Next</button>
-				) : (
-					<button type="submit">Submit</button>
-				)}
-			</form>
-		</div>
-	)
-}
+  return (
+    <div id={`step${step}`} className={`step-container ${step === 1 ? 'show' : 'hide'}`}>
+      <h2>Step {step}</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <input
+            type="text"
+            id="first_name"
+            value={formData.first_name}
+            onChange={handleChange}
+            placeholder="First Name"
+          />
+        </div>
+        <div className="input-group">
+          <input
+            type="text"
+            id="last_name"
+            value={formData.last_name}
+            onChange={handleChange}
+            placeholder="Last Name"
+          />
+        </div>
+        {step !== 1 && (
+          <button type="button" onClick={onPrev}>
+            Previous
+          </button>
+        )}
+        {step !== 3 ? (
+          <button type="submit">Next</button>
+        ) : (
+          <button type="submit">Submit</button>
+        )}
+      </form>
+    </div>
+  );
+};
 
-export default Step
+export default Step;
